8c8efe20f9847a0112721422543c9e86890ebb25
Michal Piotrowski <michal.x86@gmail.com>
2022-10-26 12:22:02 +0000

    More testcases added

--- gstreamer/motion_detector.py
+++ gstreamer/motion_detector.py
@@ -1,6 +1,25 @@
-#from pycoral.adapters.detect import BBox
+
+
+#def by_score(obj):
+#    return obj.score
 
 def get_motion_continuity(old_obj, objs):
-    for obj in objs:
-        print("obj: {}".format(obj.id))
-    return 5
+  #objs.sort(key=by_score)
+
+  new_obj = old_obj
+
+  if objs:
+    if new_obj is None:
+      new_obj = objs[0]
+      for obj in objs:
+        if new_obj.score < obj.score:
+          new_obj = obj
+#    else:
+#      fitness = 0;
+#      for obj in objs:
+#        iou(new_obj, obj)
+
+  if new_obj != old_obj:
+    new_obj = new_obj._replace(score = 0)
+
+  return new_obj
--- tests/gstreamer/test_motion_detector.py
+++ tests/gstreamer/test_motion_detector.py
@@ -5,14 +5,44 @@ import unittest
 
 class TestMotionDetector(unittest.TestCase):
 
-  def test_get_motion_continuity(self):
-    self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+  def test_get_motion_continuity_no_oldobj_no_detections(self):
+    self.assertEqual(motion_detector.get_motion_continuity(None,[]), None , "Should be None")
 
-  def test_get_motion_continuity2(self):
+  def test_get_motion_continuity_no_oldobj_1_detection(self):
     obj = mocks.Object(id=1, score=0.5, bbox=mocks.BBox(xmin=1, ymin=1, xmax=2, ymax=2))
+    exp_obj = mocks.Object(id=1, score=0.0, bbox=mocks.BBox(xmin=1, ymin=1, xmax=2, ymax=2))
 
-    l = (obj,obj)
-    self.assertEqual(motion_detector.get_motion_continuity(obj,l), 5, "Should be 5")
+    new_obj = motion_detector.get_motion_continuity(None,[obj])
+
+    self.assertEqual(new_obj, exp_obj, "Should be equal to: {}".format(exp_obj))
+    self.assertEqual(obj.score, 0.5, "Input obj should not be modified.")
+
+  def test_get_motion_continuity_no_oldobj_3_detections(self):
+    obj1 = mocks.Object(id=1, score=0.6, bbox=mocks.BBox(xmin=1, ymin=1, xmax=2, ymax=2))
+    obj2 = mocks.Object(id=2, score=0.8, bbox=mocks.BBox(xmin=3, ymin=3, xmax=4, ymax=4))
+    obj3 = mocks.Object(id=3, score=0.7, bbox=mocks.BBox(xmin=5, ymin=5, xmax=6, ymax=6))
+    exp_obj = mocks.Object(id=2, score=0.0, bbox=mocks.BBox(xmin=3, ymin=3, xmax=4, ymax=4))
+
+    new_obj = motion_detector.get_motion_continuity(None,[obj1, obj2, obj3])
+
+    self.assertEqual(new_obj, exp_obj, "Should be equal to: {}".format(exp_obj))
+
+  def test_get_motion_continuity_oldobj_no_detection(self):
+    obj = mocks.Object(id=1, score=0.5, bbox=mocks.BBox(xmin=1, ymin=1, xmax=2, ymax=2))
+    exp_obj = mocks.Object(id=1, score=0.5, bbox=mocks.BBox(xmin=1, ymin=1, xmax=2, ymax=2))
+
+    new_obj = motion_detector.get_motion_continuity(obj, None)
+
+    self.assertEqual(new_obj, exp_obj, "Should be equal to: {}".format(exp_obj))
+    self.assertEqual(obj.score, 0.5, "Input obj should not be modified.")
+
+  def test_get_motion_continuity_oldobj_1_detection_nointersection(self):
+    obj1 = mocks.Object(id=1, score=0.6, bbox=mocks.BBox(xmin=1, ymin=1, xmax=2, ymax=2))
+    obj2 = mocks.Object(id=1, score=0.8, bbox=mocks.BBox(xmin=3, ymin=3, xmax=4, ymax=4))
+
+    new_obj = motion_detector.get_motion_continuity(obj1, [obj2])
+
+    self.assertEqual(new_obj, None, "Should be None")
 
 if __name__ == '__main__':
     unittest.main()
