d4364991bb6ca2e0a9ec9c86fec73239a5735196
Michal Piotrowski <michal.x86@gmail.com>
2022-10-29 23:42:50 +0000

    Unittests for key_emitter added

--- gstreamer/key_emitter.py
+++ gstreamer/key_emitter.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 class KeyState(Enum):
     RELEASED = 0
-    PRESSING = 1
+#    PRESSING = 1
     PRESSED = 2
     REPEATING = 3
 
@@ -24,16 +24,43 @@ class KeyEvent(Enum):
     REPEAT   = 8002
     RELEASED = 8003
 
+KEY_PRESS_FIRE_PERIOD = 2000 * 1000
+
 class KeyEmitter:
     def __init__(self):
         self.state = KeyState.RELEASED
         self.raw_input = RawCode.NO_INPUT
         self.input_time = 0
 
-    def push_input(self, raw_input, time):
-        self.raw_input = raw_input
-        if time == 2030:
-            return KeyCode.UP, KeyEvent.PRESSED
-        return KeyCode.UP, KeyEvent.RELEASED
+    def rawinput_2_keycode(self, raw_input):
+        if raw_input == RawCode.NO_INPUT:
+            return KeyCode.NO_KEY
+        elif raw_input == RawCode.GEST_UP:
+            return KeyCode.UP
+        elif raw_input == RawCode.GEST_DOWN:
+            return KeyCode.DOWN
+        elif raw_input == RawCode.GEST_OK:
+            return KeyCode.OK
 
+    def push_input(self, raw_input, time):
+        ret_key_code = KeyCode.NO_KEY
+        ret_key_event = KeyEvent.NO_EVENT
+
+        if self.raw_input != raw_input:
+            self.input_time = time
+            if raw_input == RawCode.NO_INPUT and self.state in {KeyState.PRESSED, KeyState.REPEATING}:
+                ret_key_code = self.rawinput_2_keycode(self.raw_input) 
+                ret_key_event = KeyEvent.RELEASED
+            self.raw_input = raw_input
+
+        if ret_key_event != KeyEvent.RELEASED:
+            time_delta = time - self.input_time
+            
+            if raw_input != RawCode.NO_INPUT and time_delta >= KEY_PRESS_FIRE_PERIOD:
+                ret_key_code = self.rawinput_2_keycode(raw_input)
+                ret_key_event = KeyEvent.PRESSED
+                self.state = KeyState.PRESSED
+
+
+        return ret_key_code, ret_key_event
 
--- tests/gstreamer/test_key_emitter.py
+++ tests/gstreamer/test_key_emitter.py
@@ -5,20 +5,41 @@ from gstreamer.key_emitter import KeyCode
 from gstreamer.key_emitter import KeyEvent
 import unittest
 
+TIMESTAMP_START               = 2000 * 1000
+TIMESTAMP_KEY_DOWN            = 3000 * 1000
+TIMESTAMP_KEY_RELEASE_NOKEY   = 3001 * 1000
+TIMESTAMP_KEY_PRESS           = 5000 * 1000
+TIMESTAMP_KEY_RELEASE         = 5001 * 1000
+TIMESTAMP_KEY_REPEAT_01       = 7000 * 1000
+TIMESTAMP_KEY_REPEAT_02       = 8000 * 1000
+
+
 class TestKeyEmitter(unittest.TestCase):
 
     def setUp(self):
         self.emitter = KeyEmitter()
 
-    def test_push_input_press_key(self):
-        self.emitter.push_input(RawCode.NO_INPUT,2000)
+    def test_push_input_press_key_too_short(self):
+        self.emitter.push_input(RawCode.NO_INPUT, TIMESTAMP_START)
 
-        key, event = self.emitter.push_input(RawCode.GEST_UP,2030)
-        expected = (KeyCode.UP, KeyEvent.PRESSED)
+        key, event = self.emitter.push_input(RawCode.GEST_UP, TIMESTAMP_KEY_DOWN)
+        expected = (KeyCode.NO_KEY, KeyEvent.NO_EVENT)
         self.assertEqual((key, event), expected, "Got: {},{}\nShould be: {}".format(key, event, expected))
 
-        key, event = self.emitter.push_input(RawCode.NO_INPUT,2060)
-        expected = (KeyCode.UP, KeyEvent.RELEASED)
+        key, event = self.emitter.push_input(RawCode.NO_INPUT, TIMESTAMP_KEY_RELEASE_NOKEY)
+        expected = (KeyCode.NO_KEY, KeyEvent.NO_EVENT)
+        self.assertEqual((key, event), expected, "Got: {},{}\nShould be: {}".format(key, event, expected))
+
+    def test_push_input_press_key(self):
+        self.emitter.push_input(RawCode.NO_INPUT, TIMESTAMP_START)
+        self.emitter.push_input(RawCode.GEST_DOWN, TIMESTAMP_KEY_DOWN)
+
+        key, event = self.emitter.push_input(RawCode.GEST_DOWN, TIMESTAMP_KEY_PRESS)
+        expected = (KeyCode.DOWN, KeyEvent.PRESSED)
+        self.assertEqual((key, event), expected, "Got: {},{}\nShould be: {}".format(key, event, expected))
+
+        key, event = self.emitter.push_input(RawCode.NO_INPUT, TIMESTAMP_KEY_RELEASE)
+        expected = (KeyCode.DOWN, KeyEvent.RELEASED)
         self.assertEqual((key, event), expected, "Got: {},{}\nShould be: {}".format(key, event, expected))
 
 
